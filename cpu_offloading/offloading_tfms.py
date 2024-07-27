4b19090d28994a3c2afe99f3a97ba48eb12b99e1
kshitij12345 <kshitijkalambarkar@gmail.com>
2024-07-27 14:27:39 +0200

    add basic offloading tfms for llama-2

---
+++ cpu_offloading/offloading_tfms.py
@@ -0,0 +1,325 @@
+import thunder
+from thunder import Transform
+from thunder.core.proxies import TensorProxy, variableify
+from thunder.core.pytree import tree_flatten, tree_map
+from thunder.core.trace import tracectx, from_trace
+from thunder.extend import OperatorExecutor
+from thunder.core.symbol import BoundSymbol
+from thunder.core import prims
+from thunder.core.transforms import bsym_list_to_dag, Node, toposort_bsym_dag, TOPOSORT_ORDER
+
+from collections.abc import Sequence
+
+import torch
+
+offload_exec = OperatorExecutor("offload_exec")
+
+offload_stream = torch.cuda.Stream()
+
+
+def offload_to_cpu_impl(t):
+    cuda = torch.device(t.device)
+    offload_stream.wait_stream(torch.cuda.default_stream(cuda))
+    with torch.cuda.stream(offload_stream):
+        return t.to("cpu")
+
+
+offload_to_cpu = offload_exec.register_operator(
+    "offload_to_cpu",
+    meta=lambda t: TensorProxy("offloaded_" + t.name, like=t, device=thunder.core.devices.Device("cpu")),
+    fn=offload_to_cpu_impl,
+)
+
+
+def load_to_gpu_impl(t, device):
+    return t.to(device)
+
+
+load_to_gpu = offload_exec.register_operator(
+    "load_to_gpu",
+    meta=lambda t, device: TensorProxy(like=t, device=thunder.core.devices.Device(device)),
+    fn=load_to_gpu_impl,
+)
+
+
+def sync_stream_impl():
+    torch.cuda.synchronize()
+
+
+sync_stream = offload_exec.register_operator("sync_stream", meta=lambda: None, fn=sync_stream_impl)
+
+
+def get_symbols_to_last_used_variables(symbols, ignore, first_used=False):
+    """Get a mapping from symbols to the last used variables.
+
+    Mark last used intermediates to be deleted. This is necessary to avoid memory leaks.
+
+    Args:
+        symbols: list of symbols
+        ignore: list of variables to be ignored, they will not be marked as last used
+
+    Returns:
+        dict: mapping from symbols to the last used variables
+    """
+    ignore = (ignore,) if not isinstance(ignore, Sequence) else ignore
+    ignore = tree_flatten(ignore)[0]
+    variable_to_last_symbol = {}
+    symbol_to_last_variables = {}
+    symbol_to_idx = {sym: idx for idx, sym in enumerate(symbols)}
+
+    def _mark_last_use(symbol, variable):
+        if variable in ignore:
+            return
+        if not variable in variable_to_last_symbol:
+            variable_to_last_symbol[variable] = symbol
+            symbol_to_last_variables.setdefault(symbol, []).append(variable)
+
+    iter_symbols = symbols if first_used else reversed(symbols)
+    for symbol in iter_symbols:
+        # If this function is used in the combined nvfuser+torch executor, there are no symbols but regions.
+        # Regions do not have args, kwargs
+        if hasattr(symbol, "inputs"):
+            variables = tuple(symbol.inputs) + tuple(symbol.outputs)
+        else:
+            variables = (symbol.flat_variableified_proxy_args) + tuple(symbol.flat_variableified_proxy_outs)
+        tree_map(lambda x: _mark_last_use(symbol, x), variables)
+
+    return symbol_to_last_variables, variable_to_last_symbol, symbol_to_idx
+
+
+def move_loads_closer_to_consumer(execution_trace):
+    new_execution_trace = from_trace(execution_trace)
+
+    compute_producers, compute_consumers = thunder.core.utils.producers_and_consumers(execution_trace)
+
+    bsyms = execution_trace.bound_symbols
+    symbol_to_idx = {sym: idx for idx, sym in enumerate(bsyms)}
+    idx_to_swap = {}
+    symbols_to_swap = {}
+    for idx, bound_symbol in enumerate(bsyms):
+        if (
+            bound_symbol.sym.id == load_to_gpu.id
+            and (bsyms[idx + 1].sym.id) in ("reshape", "permute")
+            and bsyms[idx + 2].sym.id == prims.PrimIDs.DEL
+        ):
+            consumer_idx = symbol_to_idx[compute_consumers[bsyms[idx + 1].output][0]]
+            idx_to_swap[idx] = consumer_idx
+            symbols_to_swap[bound_symbol] = compute_consumers[bsyms[idx + 1].output][0]
+
+    new_bsyms = [bsym for bsym in bsyms]
+    for symbol, consumer_symbol in symbols_to_swap.items():
+        # print(symbol)
+        # print(consumer_symbol)
+        symbol_to_idx = {sym: idx for idx, sym in enumerate(new_bsyms)}
+        curr_idx = symbol_to_idx[symbol]
+        consumer_idx = symbol_to_idx[consumer_symbol]
+        for _ in range(3):
+            symbol_being_updated = new_bsyms.pop(curr_idx)
+            new_bsyms.insert(consumer_idx - 1, symbol_being_updated)
+
+    new_execution_trace.bound_symbols = new_bsyms
+    return new_execution_trace
+
+
+class CPUOffloading(Transform):
+    def __init__(self):
+        self.forward_pass = None
+        self.backward_pass = None
+
+    def _get_tensors_to_offload(self, forward_trace):
+        return_bsym = forward_trace.bound_symbols[-1]
+        trace_args = return_bsym.args[0]["flat_args"]
+        saved_tensors = return_bsym.args[1][0]
+
+        tensor_args_name = tuple(arg.name for arg in trace_args if isinstance(arg, TensorProxy))
+
+        def is_in_tensor_args(t):
+            return t.name in tensor_args_name
+
+        def is_cuda_tensor(t):
+            return t.device.type == "cuda"
+
+        # Tensors which are intermediate and not argument to the computation trace are
+        # the ones we are interested in offloading.
+        self.tensors_to_offload = tuple(t for t in saved_tensors if ((not is_in_tensor_args(t)) and is_cuda_tensor(t)))
+        return self.tensors_to_offload
+
+    def _replace_saved_tensors(self, forward_trace, new_output_map):
+        return_bsym = forward_trace.bound_symbols[-1]
+        return_bsym_args = return_bsym.args
+        saved_tensors = return_bsym.args[1][0]
+
+        new_saved_tensors = []
+        for t in saved_tensors:
+            new_output = new_output_map.get(variableify(t), t)
+            new_saved_tensors.append(new_output)
+
+        new_return_bsym = BoundSymbol.from_bsym(
+            return_bsym, **{"args": (return_bsym_args[0], (tuple(new_saved_tensors), return_bsym_args[1][1]))}
+        )
+        forward_trace.bound_symbols.pop(-1)
+        forward_trace.bound_symbols.append(new_return_bsym)
+
+    def _offload_tensors_from_forward(self, computation_trace):
+        # Find the tensors to offload.
+        # We offload saved tensors which are not arguments to the computation trace and are saved for backwards.
+        tensors_to_offload = self._get_tensors_to_offload(computation_trace)
+        _, variable_to_last_symbol, symbol_to_idx = get_symbols_to_last_used_variables(
+            computation_trace.bound_symbols[:-1], ()
+        )
+
+        # Insert the offloading calls after the last use of the saved tensor.
+        new_output_map = {}
+        new_output_dev_map = {}
+        for idx, t in enumerate(
+            sorted(tensors_to_offload, key=lambda t: symbol_to_idx[variable_to_last_symbol[variableify(t)]])
+        ):
+            last_used_symbol = variable_to_last_symbol[variableify(t)]
+            last_used_symbol_idx = symbol_to_idx[last_used_symbol]
+
+            computation_trace.push_scope([])
+            with tracectx(computation_trace):
+                o = offload_to_cpu(t)
+                prims.python_del(t)
+            scoped_comp = computation_trace.pop_scope()
+            scoped_comp[0].header = "Created by CPU Offloading Transform"
+            # print(scoped_comp)
+            computation_trace.bound_symbols.insert(last_used_symbol_idx + 1 + (idx * 2), scoped_comp[1])
+            computation_trace.bound_symbols.insert(last_used_symbol_idx + 1 + (idx * 2), scoped_comp[0])
+
+            new_output_map[variableify(t)] = o
+            new_output_dev_map[variableify(t)] = t.device.device_str()
+
+        self._replace_saved_tensors(computation_trace, new_output_map)
+        self._offloaded_tensors = new_output_map
+        self._offloaded_tensors_dev = new_output_dev_map
+
+    def transform_trace_post_optimization(self, computation_trace: thunder.TraceCtx, **kwargs):
+        if self.forward_pass is None:
+            self.forward_pass = computation_trace
+            # Processing for the forward pass.
+            if "augmented_forward" in computation_trace.fn.__name__:
+                self._offload_tensors_from_forward(computation_trace)
+        else:
+            self.backward_pass = computation_trace
+            offloaded_tensors = self._offloaded_tensors
+            offloaded_tensors_dev_map = self._offloaded_tensors_dev
+
+            if len(offloaded_tensors) == 0:
+                return computation_trace
+
+            _, variable_to_first_symbol, symbol_to_idx = get_symbols_to_last_used_variables(
+                computation_trace.bound_symbols, (), first_used=True
+            )
+
+            for t in offloaded_tensors.keys():
+                pass
+            symbol_to_idx[variable_to_first_symbol[t]]
+
+            offset = symbol_to_idx[variable_to_first_symbol[t]]
+            # Unpack Collection
+            unpack_sym = variable_to_first_symbol[t]
+            unpack_sym_out = unpack_sym.output
+            new_out = []
+            for out in unpack_sym_out:
+                vout = variableify(out)
+                if vout in offloaded_tensors:
+                    new_out.append(offloaded_tensors[vout])
+                else:
+                    new_out.append(out)
+            new_unpack_bsym = BoundSymbol.from_bsym(unpack_sym, output=tuple(new_out))
+            computation_trace.bound_symbols[offset] = new_unpack_bsym
+
+            offset = offset + 1
+            _, variable_to_first_symbol, symbol_to_idx = get_symbols_to_last_used_variables(
+                computation_trace.bound_symbols[offset:], (), first_used=True
+            )
+
+            # Load to GPU before usage.
+            # Should iterate in correct order (else it would be problematic).
+            for idx, (vt, offloaded_t) in enumerate(
+                sorted(offloaded_tensors.items(), key=lambda kv: symbol_to_idx[variable_to_first_symbol[kv[0]]])
+            ):
+                first_used_symbol = variable_to_first_symbol[vt]
+                first_used_symbol_idx = symbol_to_idx[first_used_symbol]
+                t = vt.proxy
+                device = offloaded_tensors_dev_map[vt]
+                with tracectx(computation_trace):
+                    new_sym = load_to_gpu.bind(offloaded_t, device, output=t)
+                new_sym.header = "Created by CPU Offloading Transform"
+                computation_trace.bound_symbols.insert(offset + first_used_symbol_idx + idx, new_sym)
+
+            computation_trace = move_loads_closer_to_consumer(computation_trace)
+            sync_bsym = sync_stream.bind(output=None)
+            sync_bsym.header = "Inserted by CPU Offloading"
+            computation_trace.bound_symbols.insert(0, sync_bsym)
+
+        return computation_trace
+
+
+dim = 2048
+
+
+class Model(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = torch.nn.Linear(dim, dim)
+        self.fc2 = torch.nn.Linear(dim, dim)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = torch.nn.functional.relu(x)
+        x = self.fc2(x)
+        return x
+
+
+with torch.device("cuda"):
+    model = Model()
+    x = torch.randn(dim, dim)
+    args = (x,)
+    kwargs = {}
+
+from thunder.benchmarks.targets import LitGPTConfig, LitGPTBenchmark
+
+with torch.device("cuda"):
+    cfg: LitGPTConfig = LitGPTConfig.from_name("Llama-2-7b-hf")
+    cfg.n_layer = 10
+    cfg.block_size = 1024
+    b = LitGPTBenchmark(cfg)
+    model = b.fn()
+    args, kwargs = b.make_batch()
+
+offload_tfms = CPUOffloading()
+print(torch.cuda.max_memory_allocated() / 1e9)
+jmodel = thunder.jit(model, transforms=[offload_tfms])
+# jmodel = thunder.jit(model)
+
+o = jmodel(*args, **kwargs)
+print(torch.cuda.max_memory_allocated() / 1e9)
+
+o.sum().backward()
+
+print(torch.cuda.max_memory_allocated() / 1e9)
+# print(thunder.last_backward_traces(jmodel)[-1])
+
+# 5 Layer
+# Without TFMS
+# 5.148185088
+# 23.921770496
+# 25.628853248
+
+# With TFMS
+# 5.148185088
+# 7.798521856
+# 15.084094464
+
+# 10 Layer
+# Without TFMS
+# 9.195852288
+# 45.460013056
+# 47.167095808
+
+# With TFMS
+# 9.195852288
+# 11.846189056
+# 24.500470784
